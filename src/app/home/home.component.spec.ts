c41ca966d1a624f666c6835ff8269d6338e0958f
Joshua Morony <joshua.morony@gmail.com>
2023-09-05 12:41:05 +0930

    feat: render item for each gif

---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import HomeComponent from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HomeComponent],
+    })
+      .overrideComponent(HomeComponent, {
+        remove: { imports: [] },
+        add: { imports: [] },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/home/ui/gif-list.component.ts
+++ src/app/home/ui/gif-list.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Gif } from 'src/app/shared/interfaces';
 
 @Component({
   standalone: true,
   selector: 'app-gif-list',
-  template: ` <p>Hello world</p> `,
+  template: `
+    <ul>
+      <li
+        *ngFor="let gif of gifs; trackBy: trackByFn"
+        data-testid="gif-list-item"
+      ></li>
+    </ul>
+  `,
+  imports: [CommonModule],
 })
 export class GifListComponent {
   @Input({ required: true }) gifs!: Gif[];
+
+  trackByFn(_: number, gif: Gif) {
+    return gif.permalink;
+  }
 }
