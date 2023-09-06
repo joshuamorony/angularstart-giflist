23b819ba51521de1e97dea9117947e3b5c7d1c7b
Joshua Morony <joshua.morony@gmail.com>
2023-09-07 09:25:43 +0930

    test: initial request sets gifs from subreddit

--- src/app/shared/data-access/reddit.service.spec.ts
+++ src/app/shared/data-access/reddit.service.spec.ts
@@ -1,18 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { RedditService } from './reddit.service';
 
 describe('RedditService', () => {
   let service: RedditService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [RedditService],
     });
 
     service = TestBed.inject(RedditService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should create', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('source: gifsLoaded$', () => {
+    const mockData = [{}, {}, {}] as any;
+
+    it('should set gifs on initial load from gifs subreddit', () => {
+      const request = httpMock.expectOne('/r/gifs/hot/.json');
+      request.flush(mockData);
+
+      expect(service.gifs()).toEqual(mockData);
+    });
+
+    // it('should set gifs from subreddit when control value changes', () => {});
+    // it('should set error if request fails', () => {});
   });
 });
