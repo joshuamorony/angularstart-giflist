032acf924dd9e75de40600964a2b9357ed7810e0
Joshua Morony <joshua.morony@gmail.com>
2023-09-08 20:25:45 +0930

    feat: toggle playing and pausing of gif

--- src/app/home/ui/gif-player.component.ts
+++ src/app/home/ui/gif-player.component.ts
@@ -1,10 +1,27 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subject, tap } from 'rxjs';
+
+interface GifPlayerState {
+  playing: boolean;
+  loaded: boolean;
+}
 
 @Component({
   standalone: true,
   selector: 'app-gif-player',
   template: `
     <video
+      (click)="togglePlay$.next()"
+      #gifPlayer
       playsinline
       poster="none"
       preload="none"
@@ -16,11 +33,36 @@ import { Component, Input } from '@angular/core';
 })
 export class GifPlayerComponent {
   @Input({ required: true }) src!: string;
+  @ViewChild('gifPlayer') video!: ElementRef<HTMLVideoElement>;
 
-  // state
-  // loading | loaded | playing
+  state = signal<GifPlayerState>({
+    playing: false,
+    loaded: false,
+  });
+
+  //selectors
+  playing = computed(() => this.state().playing);
+  loaded = computed(() => this.state().loaded);
 
   // sources
-  // play subject
+  togglePlay$ = new Subject<void>();
   // stream from loadeddata event listener
+
+  constructor() {
+    //reducers
+    this.togglePlay$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() =>
+        this.state.update((state) => ({ ...state, playing: !state.playing }))
+      );
+
+    effect(() => {
+      const video = this.video?.nativeElement;
+      const playing = this.playing();
+
+      if (!video) return;
+
+      playing ? video.play() : video.pause();
+    });
+  }
 }
