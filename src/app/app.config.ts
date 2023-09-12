604415077d7cdb81971d933c28e44a88880785f7
Joshua Morony <joshua.morony@gmail.com>
2023-09-12 09:32:15 +0930

    chore: ng add @angular/material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)],
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), provideAnimations()],
 };
