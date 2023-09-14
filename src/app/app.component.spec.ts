143545d847989800bd2b8bf8c942c1fd47a9c3c8
Joshua Morony <joshua.morony@gmail.com>
2023-09-14 10:20:13 +0930

    test: should launch snack bar with error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,54 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RedditService } from './shared/data-access/reddit.service';
+import { signal } from '@angular/core';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
+  const mockErrorSignal = signal<string | null>(null);
+  let snackBar: MatSnackBar;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [AppComponent],
-    })
-  );
+      providers: [
+        {
+          provide: RedditService,
+          useValue: {
+            error: mockErrorSignal,
+          },
+        },
+        {
+          provide: MatSnackBar,
+          useValue: {
+            open: jest.fn(),
+          },
+        },
+      ],
+    });
+
+    snackBar = TestBed.inject(MatSnackBar);
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  describe('effects', () => {
+    it('should open snack bar with error state when error changes', () => {
+      const testError = 'some error';
+      mockErrorSignal.set(testError);
+
+      expect(snackBar.open).toHaveBeenCalledWith(testError);
+    });
+    it('should not open snack bar for null error messages', () => {
+      const testError = 'some error';
+      mockErrorSignal.set(testError);
+      mockErrorSignal.set(null);
+
+      expect(snackBar.open).toHaveBeenCalledTimes(1);
+    });
   });
 });
