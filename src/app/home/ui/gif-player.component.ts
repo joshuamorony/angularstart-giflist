44ca896a5a007a67cffbd307c4afac21923d9363
Joshua Morony <joshua.morony@gmail.com>
2023-10-25 09:31:45 +1030

    refactor: use connect for gif player

--- src/app/home/ui/gif-player.component.ts
+++ src/app/home/ui/gif-player.component.ts
@@ -10,12 +10,15 @@ import {
 } from '@angular/core';
 import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { connect } from 'ngxtension/connect';
 import {
   EMPTY,
   Subject,
   combineLatest,
   filter,
   fromEvent,
+  map,
+  merge,
   switchMap,
 } from 'rxjs';
 
@@ -131,23 +134,14 @@ export class GifPlayerComponent {
 
   constructor() {
     //reducers
-    this.videoLoadStart$
-      .pipe(takeUntilDestroyed())
-      .subscribe(() =>
-        this.state.update((state) => ({ ...state, status: 'loading' }))
-      );
+    const nextState$ = merge(
+      this.videoLoadStart$.pipe(map(() => ({ status: 'loading' as const }))),
+      this.videoLoadComplete$.pipe(map(() => ({ status: 'loaded' as const })))
+    );
 
-    this.videoLoadComplete$
-      .pipe(takeUntilDestroyed())
-      .subscribe(() =>
-        this.state.update((state) => ({ ...state, status: 'loaded' }))
-      );
-
-    this.togglePlay$
-      .pipe(takeUntilDestroyed())
-      .subscribe(() =>
-        this.state.update((state) => ({ ...state, playing: !state.playing }))
-      );
+    connect(this.state)
+      .with(nextState$)
+      .with(this.togglePlay$, (state) => ({ playing: !state.playing }));
 
     // effects
     effect(() => {
