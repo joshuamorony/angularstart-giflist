9eca67cbcf4650e59d3a9643724d15da28bbc442
Joshua Morony <joshua.morony@gmail.com>
2024-05-27 07:29:13 +0930

    refactor: provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,15 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule, MatSnackBarModule),
+    provideHttpClient(),
+    importProvidersFrom(MatSnackBarModule),
     provideAnimations(),
   ],
 };
